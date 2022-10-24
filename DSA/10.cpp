f81387117fd4c3ca568c5303c40763c42f76677d
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-10-24 14:49:59 +0530

    Solve the Problem

---
+++ DSA/10.cpp
@@ -0,0 +1,12 @@
+/*
+    Problem Code:   SNDMA
+        Problem Statement
+    Write a program that accepts sets of three numbers, and prints the second-maximum number among the three.
+  Input
+    First line contains the number of triples, N.
+    The next N lines which follow each have three space separated integers.
+  Output
+    For each of the N triples, output one new line which contains the second-maximum integer among the three.
+*
+*
+* */
